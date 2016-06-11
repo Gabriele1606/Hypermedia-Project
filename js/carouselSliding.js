26d42e904e91b142f1f33da71e19fad2af10ece7
Gabriele Bressan <gabri.bressan@gmail.com>
2016-06-11 12:29:03 +0200

    fixed carousel auto sliding

--- js/carouselSliding.js
+++ js/carouselSliding.js
@@ -1,5 +1,9 @@
-
+/*
+ * Used for find your shop carousel
+ */
   var $ = jQuery.noConflict();
   $(document).ready(function() { 
       $('#map-slider').carousel({ interval: 7000, cycle: true });
   }); 
+
+
